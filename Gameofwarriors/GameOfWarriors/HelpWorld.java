c5e4f5b80d312c03a2d9c7ad5ba9126f73a6af90
ShaluRani <shalu.rani@sjsu.edu>
2018-11-29 11:26:28 -0800

    Added characters in Helpworld. Test commit

--- Gameofwarriors/GameOfWarriors/HelpWorld.java
+++ Gameofwarriors/GameOfWarriors/HelpWorld.java
@@ -1,20 +1,43 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class HelpWorld here.
+ * Write a description of class MyWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class HelpWorld extends World
+public class HelpWorld extends World 
 {
-
+    Aladdin aladdin = new Aladdin();
+    //private BackOption backOption ;
+    //private BackCommand backCmd ;
+    //Start Sound
+    GreenfootSound background;
     /**
-     * Constructor for objects of class HelpWorld.
+     * Constructor for objects of class MyWorld.
      * 
      */
     public HelpWorld()
     {    
-        super(1153, 800, 1);     
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1153, 800, 1);
+        addObject(aladdin,1045, 588);
+        addObject(new Princess(),115, 594);
+        //backOption  = new BackOption("Back to Main Menu") ;
+        //backCmd = new BackCommand();
+        /*addObject(backOption,150,100);
+        backOption.setCommand(backCmd);
+        backCmd.setReceiver(
+                new IMenuReceiver() {
+                    /** Command Action */
+                    /*public void doAction() {
+                        if(Greenfoot.mouseClicked(backOption)){
+                            World gameMenuWorld = new GameMenuWorld();
+                            Greenfoot.setWorld(gameMenuWorld);
+                        }
+                    }
+              }
+              ) ;*/
     }
+
 }
